32e3a71357edbeae1ec878d71c7a18e0e73c1655
Alan A2N Bouteiller <a2n.dev@pm.me>
2023-07-20 22:12:44 +0200

    ✨ map the setting into the plugin

--- move-label.js
+++ move-label.js
@@ -1,3 +1,15 @@
+/**
+ * a small wrapper for the error, throw a `console.error`
+ * @param error error message in string
+ */
+function onLabelError(error) {
+    console.error(`IPP - on error: ${error}`);
+}
+
+/**
+ * move the label to the right place
+ * @param observerML the observer, here for shutting it off after the work is done
+ */
 function moveTargetToLz(observerML) {
     const target = document.getElementsByClassName('label-stack');
     console.info('ML -', target.length, 'target found');
@@ -23,9 +35,21 @@ function moveTargetToLz(observerML) {
     }
 }
 
-function enableObs() {
+/**
+ * set up and enable the observer
+ */
+function enableLabelObs() {
     const observerML = new MutationObserver(() => moveTargetToLz(observerML));
     observerML.observe(document.body, {childList: true, subtree: true});
 }
 
-enableObs();
+/**
+ * grant clearance of the setting say so
+ */
+function clearanceForLabel() {
+    browser.storage.sync.get('label').then((value) => {
+        if (value.label === 'true') enableLabelObs();
+    }, onLabelError)
+}
+
+clearanceForLabel();
--- remove-pbtn.js
+++ remove-pbtn.js
@@ -1,4 +1,16 @@
-const findAndDestroy = () => {
+/**
+ * a small wrapper for the error, throw a `console.error`
+ * @param error error message in string
+ */
+function onPremiumError(error) {
+    console.error(`IPP - on error: ${error}`);
+}
+
+/**
+ * find the button and remove it and disconnect the observer
+ * @param observerRPBTN the observer, here for shutting it off after the work is done
+ */
+const findAndDestroy = (observerRPBTN) => {
     const element = document.getElementsByClassName('topnav-list');
     if (element.length > 0 && element[0]) {
         console.info('RPBTN - target area found', element[0]);
@@ -14,5 +26,21 @@ const findAndDestroy = () => {
     }
 }
 
-const observerRPBTN = new MutationObserver(findAndDestroy);
-observerRPBTN.observe(document.body, {childList: true, subtree: true});
+/**
+ * set up and enable the observer
+ */
+function enablePremiumObs() {
+    const observerRPBTN = new MutationObserver(() => findAndDestroy(observerRPBTN));
+    observerRPBTN.observe(document.body, {childList: true, subtree: true});
+}
+
+/**
+ * grant clearance of the setting say so
+ */
+function clearanceForPremium() {
+    browser.storage.sync.get('premium').then((value) => {
+        if (value.premium === 'true') enablePremiumObs();
+    }, onPremiumError)
+}
+
+clearanceForPremium();
